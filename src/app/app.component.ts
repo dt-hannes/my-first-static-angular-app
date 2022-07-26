219cca3f2f86ec751c447ea25cb5cc0b5bcb08ad
dt-hannes <hannes.s@dreamtec.co.za>
2022-07-26 16:40:10 +0200

    Add API function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello, {{value}}</div>`,
+  template: `<div>Hello, {{value}}</div><div>{{apiMessage}}</div>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   value = 'Azure Static Web Apps';
+  apiMessage = "";
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get("/api/StaticAppHttpTriggerTest").subscribe(
+      (response: any) => this.apiMessage = response.text,
+      (error: any) => console.error(JSON.stringify(error)));
+  }
 }
