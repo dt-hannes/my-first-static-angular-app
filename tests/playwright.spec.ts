3c78e90c6b18d35bd1408c74e0ff9f800685fbe4
dt-hannes <hannes.s@dreamtec.co.za>
2022-07-26 14:52:08 +0200

    Update test

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('/');
   await page.waitForSelector('div');
-  await expect(page.locator('div')).toContainText('Hello World');
+  await expect(page.locator('div')).toContainText('Hello, Azure Static Web Apps');
 })
